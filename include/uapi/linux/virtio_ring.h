03b6c52532cc22428f7b2b03fa3f1b30d988cf8e
ferrum0215 <ferrum0215@postech.ac.kr>
2025-07-16 04:23:42 +0000

    fpermissive error solve

--- include/uapi/linux/virtio_ring.h
+++ include/uapi/linux/virtio_ring.h
@@ -200,9 +200,9 @@ static inline void vring_init(struct vring *vr, unsigned int num, void *p,
 			      unsigned long align)
 {
 	vr->num = num;
-	vr->desc = (struct lkl_vring_desc*)p;
+	vr->desc = (struct vring_desc *)p;
 	vr->avail = (struct vring_avail *)((char *)p + num * sizeof(struct vring_desc));
-	vr->used = (struct lkl_vring_desc*)(((uintptr_t)&vr->avail->ring[num] + sizeof(__virtio16)
+	vr->used = (struct vring_used *)(((uintptr_t)&vr->avail->ring[num] + sizeof(__virtio16)
 		+ align-1) & ~(align - 1));
 }
 
